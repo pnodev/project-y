eafcd3fb881d2bd31381032bb274a67e8a441416
Philipp Nowinski <mail@pno.dev>
2025-07-21 17:37:02 +0200

    fix: fix index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { ComboBox } from "~/components/ComboBox";
-
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [selected, setSelected] = useState<string[]>([]);
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <ComboBox
-        value={selected}
-        onChange={setSelected}
-        items={[
-          {
-            value: "next.js",
-            label: "Next.js",
-          },
-          {
-            value: "sveltekit",
-            label: "SvelteKit",
-          },
-          {
-            value: "nuxt.js",
-            label: "Nuxt.js",
-          },
-          {
-            value: "remix",
-            label: "Remix",
-          },
-          {
-            value: "astro",
-            label: "Astro",
-          },
-        ]}
-      />
+      Welcome to Project Y
     </div>
   );
 }
