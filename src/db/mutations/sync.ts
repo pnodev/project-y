8138356529f2794dd7637a91af454c8cc580a6f0
Philipp Nowinski <mail@pno.dev>
2025-08-07 15:39:00 +0200

    feat: switch to new sync engine

--- src/db/mutations/sync.ts
+++ src/db/mutations/sync.ts
@@ -4,7 +4,7 @@ export async function sync(topic: string, payload: any) {
   const appId = "676bb0d1-942d-465a-a706-4ee451177507";
   const key = "OIV3S2Q-ZMPEFQI-RHMNAVY-F4UZDDA";
   await fetch(
-    `https://connect.sync.pno.dev/stream/${appId}?key=${key}&topic=${topic}`,
+    `https://sync-connect.pno.dev/stream/${appId}?key=${key}&topic=${topic}`,
     {
       method: "POST",
       headers: {
--- src/routes/_signed-in/dashboard.tsx
+++ src/routes/_signed-in/dashboard.tsx
@@ -6,21 +6,5 @@ export const Route = createFileRoute("/_signed-in/dashboard")({
 });
 
 function RouteComponent() {
-  useEffect(() => {
-    const appId = "676bb0d1-942d-465a-a706-4ee451177507";
-    const topic = "task-update";
-    const sse = new EventSource(
-      `https://connect.sync.pno.dev/stream/${appId}?topic=${topic}`
-    );
-    sse.addEventListener("update", (event) => {
-      const { payload } = JSON.parse(event.data);
-      console.log("data", JSON.parse(payload));
-    });
-
-    return () => {
-      sse.close();
-    };
-  }, []);
-
   return <div>Hello "/_signed-in/dashboard"!</div>;
 }
